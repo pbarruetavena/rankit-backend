3604034dfd4d38f84e5857949cfc80eac110c149
pbarruetavena <pbarruetavenavieira@gmail.com>
2025-02-06 12:38:10 -0300

    conectando ao repositorio remoto

---
+++ rankit/src/main/java/com/rankit/RankitApplication.java
@@ -0,0 +1,13 @@
+package com.rankit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RankitApplication.class, args);
+	}
+
+}
---
+++ rankit/src/test/java/com/rankit/RankitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rankit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
